9b9a53b7fa5386f792d17fc2ea60a1eb36ddd78f
codewithTamuno <teachwithcode@gmail.com>
2021-07-24 18:44:06 +0100

    added expenses

---
+++ expenses.js
@@ -0,0 +1,16 @@
+var check_disable;
+
+function disable(){
+  check_disable = document.getElementById("disable").disabled = "disabled";
+  
+  /*if (check_disable){ */
+  if(check_disable){
+   
+    check_disable = null;
+  }
+  
+  
+ /* if(check_disable){
+    check_disable = document.getElementById("disable").disabled = "disabled"
+  }*/
+}a
