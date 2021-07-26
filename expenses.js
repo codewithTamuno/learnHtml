6e8ead1d9a612951e7be22780b7d88df4a35fea7
codewithTamuno <teachwithcode@gmail.com>
2021-07-26 23:27:07 +0100

    updated both expenses.html and js

--- expenses.js
+++ expenses.js
@@ -42,3 +42,103 @@ function edit3(){
   item = document.getElementById("item3").disabled = null;
   price = document.getElementById("price3").disabled = null;
 }
+
+
+
+function save4() {
+  item = document.getElementById("item4").disabled = "disabled";
+  price = document.getElementById("price4").disabled = "disabled";
+}
+
+function edit4() {
+  // for item 4.
+  item = document.getElementById("item4").disabled = null;
+  price = document.getElementById("price4").disabled = null;
+}
+
+
+function save5() {
+  item = document.getElementById("item5").disabled = "disabled";
+  price = document.getElementById("price5").disabled = "disabled";
+}
+
+function edit5() {
+  // for item 5.
+  item = document.getElementById("item5").disabled = null;
+  price = document.getElementById("price5").disabled = null;
+}
+
+function save6() {
+  item = document.getElementById("item6").disabled = "disabled";
+  price = document.getElementById("price6").disabled = "disabled";
+}
+
+function edit6() {
+  // for item 6.
+  item = document.getElementById("item6").disabled = null;
+  price = document.getElementById("price6").disabled = null;
+}
+
+function save7() {
+  item = document.getElementById("item7").disabled = "disabled";
+  price = document.getElementById("price7").disabled = "disabled";
+}
+
+function edit7() {
+  // for item 7.
+  item = document.getElementById("item7").disabled = null;
+  price = document.getElementById("price7").disabled = null;
+}
+
+function save8() {
+  item = document.getElementById("item8").disabled = "disabled";
+  price = document.getElementById("price8").disabled = "disabled";
+}
+
+function edit8() {
+  // for item 8.
+  item = document.getElementById("item8").disabled = null;
+  price = document.getElementById("price8").disabled = null;
+}
+
+function save9() {
+  item = document.getElementById("item9").disabled = "disabled";
+  price = document.getElementById("price9").disabled = "disabled";
+}
+
+function edit9() {
+  // for item 9.
+  item = document.getElementById("item9").disabled = null;
+  price = document.getElementById("price9").disabled = null;
+}
+
+
+var price1;
+var price2;
+var price3;
+var price4;
+var price5;
+var price6;
+var price7;
+var price8;
+var price9;
+
+function total_price(){
+  price1 = document.getElementById("price1").value;
+  price2 = document.getElementById("price2").value;
+  price3 = document.getElementById("price3").value;
+  price4 = document.getElementById("price4").value;
+  price5 = document.getElementById("price5").value;
+  price6 = document.getElementById("price6").value;
+  price7 = document.getElementById("price7").value;
+  price8 = document.getElementById("price8").value;
+  price9 = document.getElementById("price9").value;
+  
+  var total = [price1, price2, price3, price4, price5, price6, price7, price8, price9];
+  for (prices in total){
+    var price = parseInt(prices) + parseInt(prices);
+  }
+  
+  document.getElementById("total_price").value = parseInt(price);
+  
+}
